03d3b31ea3c2a2086e62e5c196e36c2a474ccc93
Rupen Bhimani <rupenbhimani109@gmail.com>
2022-05-14 15:15:28 +0530

    update products imgs

--- src/Sections/ImageGallery/Components/ImagesGalleryData.js
+++ src/Sections/ImageGallery/Components/ImagesGalleryData.js
@@ -1,66 +1,66 @@
 export const photos = [
   {
-    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    src: "https://3.imimg.com/data3/PF/RX/MY-4603137/conveyor-attachment-chain-500x500.jpg",
     width: 4,
     height: 3,
   },
   {
-    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    src: "https://2.imimg.com/data2/AP/DR/MY-4603137/chain-conveyor-systems-250x250.jpg",
     width: 1,
     height: 1,
   },
   {
-    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    src: "https://2.imimg.com/data2/MX/UP/MY-4603137/conveyor-chains-250x250.jpg",
     width: 3,
     height: 4,
   },
   {
-    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    src: "https://3.imimg.com/data3/GV/WU/MY-4603137/steel-belt-conveyors-250x250.jpg",
+    width: 3,
+    height: 2.5,
+  },
+  {
+    src: "https://3.imimg.com/data3/XI/NM/MY-4603137/hinged-steel-belt-conveyors-250x250.jpeg",
     width: 3,
     height: 4,
   },
   {
-    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    src: "https://3.imimg.com/data3/CM/JR/MY-4603137/centralized-conveyors-belt-250x250.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://3.imimg.com/data3/YY/HL/MY-4603137/dry-steel-belt-conveyor-250x250.jpeg",
     width: 3,
     height: 4,
   },
   {
-    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    src: "https://3.imimg.com/data3/VI/CI/MY-4603137/cnc-turning-machine-conveyors-250x250.jpg",
     width: 4,
     height: 3,
   },
   {
-    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    // src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    // src: "https://source.unsplash.com/XiDA78wAZVw/600x799",
     width: 3,
     height: 4,
   },
   {
-    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    // src: "https://source.unsplash.com/x8xJpClTvR0/800x599",
     width: 4,
     height: 3,
   },
   {
-    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    // src: "https://source.unsplash.com/qGQNmBE7mYw/800x599",
     width: 4,
     height: 3,
   },
   {
-    src: "https://source.unsplash.com/XiDA78wAZVw/600x799",
-    width: 3,
-    height: 4,
-  },
-  {
-    src: "https://source.unsplash.com/x8xJpClTvR0/800x599",
-    width: 4,
-    height: 3,
-  },
-  {
-    src: "https://source.unsplash.com/qGQNmBE7mYw/800x599",
-    width: 4,
-    height: 3,
-  },
-  {
-    src: "https://source.unsplash.com/NuO6iTBkHxE/800x599",
+    // src: "https://source.unsplash.com/NuO6iTBkHxE/800x599",
     width: 4,
     height: 3,
   },
