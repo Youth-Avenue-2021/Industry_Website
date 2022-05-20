ac67e9b89ff9bbbd8a158d9f0c5cc3e53047225d
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-05-20 16:57:39 +0530

    navbar company logo

--- src/Components/Navigation/Navbar.js
+++ src/Components/Navigation/Navbar.js
@@ -2,51 +2,51 @@ import { AnimatePresence, motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import NavbarLinks from "./NavbarLinks";
-import Logo from "../../Assets/productImgs/ShreebaiLogo.png"
+import Logo from "../../Assets/productImgs/ShreebaiLogo.png";
 
 const Navbar = () => {
-  const [active, setActive] = useState(false);
-  const location = useLocation();
+    const [active, setActive] = useState(false);
+    const location = useLocation();
 
-  useEffect(() => {
-    setActive(false);
-  }, [location.pathname]);
+    useEffect(() => {
+        setActive(false);
+    }, [location.pathname]);
 
-  const animation = {
-    initial: { opacity: 0, y: 0 },
-    animate: { opacity: 1, y: 0, transition: { duration: 0.4 } },
-    exit: { opacity: 0, y: 0, transition: { duration: 0.4 } },
-  };
-  return (
-    <>
-      <div className="z-40 flex flex-col items-center justify-around py-4 bg-yellowColor md:flex-row">
-        <div className="flex flex-row items-center justify-around w-full md:w-auto md:justify-center">
-          <img className="h-12 w-12 p-1" src={Logo} alt=""></img>
-          <Link to="/" className="text-2xl font-headingFont">
-            ShreeBai Industries
-          </Link>
-          <div
-            onClick={() => {
-              active ? setActive(false) : setActive(true);
-            }}
-            className="flex items-center justify-center cursor-pointer md:hidden"
-          >
-            <i className="fa-solid fa-bars-staggered"></i>
-          </div>
-        </div>
-        <div className="flex-row items-center justify-center hidden md:flex">
-          <NavbarLinks />
-        </div>
-      </div>
-      <AnimatePresence exitBeforeEnter>
-        {active && (
-          <motion.div variants={animation} initial="initial" animate="animate" exit="exit" className="absolute z-20 flex flex-col items-center justify-center w-full pb-8 bg-yellowColor md:pb-0 md:flex-row">
-            <NavbarLinks />
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </>
-  );
+    const animation = {
+        initial: { opacity: 0, y: 0 },
+        animate: { opacity: 1, y: 0, transition: { duration: 0.4 } },
+        exit: { opacity: 0, y: 0, transition: { duration: 0.4 } },
+    };
+    return (
+        <>
+            <div className="z-40 flex flex-col items-center justify-around py-4 bg-yellowColor md:flex-row">
+                <div className="flex flex-row items-center justify-around w-full md:w-auto md:justify-center">
+                    <Link to="/" className="text-2xl font-headingFont flex flex-row items-center">
+                        <img className="h-12 w-12 p-1 mr-2" src={Logo} alt="" />
+                        <h1>ShreeBai Industries</h1>
+                    </Link>
+                    <div
+                        onClick={() => {
+                            active ? setActive(false) : setActive(true);
+                        }}
+                        className="flex items-center justify-center cursor-pointer md:hidden"
+                    >
+                        <i className="fa-solid fa-bars-staggered"></i>
+                    </div>
+                </div>
+                <div className="flex-row items-center justify-center hidden md:flex">
+                    <NavbarLinks />
+                </div>
+            </div>
+            <AnimatePresence exitBeforeEnter>
+                {active && (
+                    <motion.div variants={animation} initial="initial" animate="animate" exit="exit" className="absolute z-20 flex flex-col items-center justify-center w-full pb-8 bg-yellowColor md:pb-0 md:flex-row">
+                        <NavbarLinks />
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </>
+    );
 };
 
 export default Navbar;
