bec3f6c6070e16dfed8b38276125d0a70c8b1c0a
Rupen Bhimani <rupenbhimani109@gmail.com>
2022-05-20 16:37:56 +0530

    add pdf

--- src/Components/Navigation/NavbarLinks.js
+++ src/Components/Navigation/NavbarLinks.js
@@ -1,11 +1,12 @@
 import SinglePageNavbarLink from "./SinglePageNavbarLink";
+import pdf from "../../Assets/Pdf/conveyor book and report.pdf"
 
 const NavbarLinks = () => {
   return (
     <>
       <SinglePageNavbarLink name="Home" linkTo="/" />
       <SinglePageNavbarLink name="Products Gallery" linkTo="/products" />
-      <SinglePageNavbarLink name="Privacy Policy" linkTo="/" />
+      <SinglePageNavbarLink name="Privacy Policy" linkTo="/public/Pdf/conveyor book and report.pdf" />
       <SinglePageNavbarLink name="Contact Us" linkTo="/contact" />
     </>
   );
